b5ce97b6cd93f8b877ab3135633bd820bd25c67a
Revaldo Gemino Kantana Sagala <aldogksagala@gmail.com>
2025-09-19 22:46:31 +0700

    remove index.html and test.js; add new note diagram

--- part0/test.js
+++
@@ -1,7 +0,0 @@
-const testDiv = document.getElementById("test");
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then(res => res.json())
-  .then(data => testDiv.innerHTML = `
-      <p>${data.activity}</p>
-    `)
-  .catch(err => console.error(err));
